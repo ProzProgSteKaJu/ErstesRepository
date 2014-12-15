cfb115ca6614282e7689fefdcd537f781c211442
judith <ju31094@gmail.com>
2014-12-15 18:31:33 +0100

    neues Programm

--- testat8.c
+++ testat8.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "Header.h"
 
-int main()
-{
-/*Blabla*/
-    return 0;
+char *print_postfix(char string[]){
+
+//Determine length of prefn
+
 }
+
+int main(){
+char prefixnotation[MAX_LENGTH];
+
+//Reading the input in prefixnotation
+printf("Geben Sie bitte einen Code in Praefixnotation ein:\n");
+gets(prefixnotation);
+
+//Calculation and Output of the postfixnotation
+printf("Dies ist der Code in Postfixnotation:\n");
+print_postfix(prefixnotation);
+
+return 0;
