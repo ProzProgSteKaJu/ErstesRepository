d373a762949f4489045115f7611350041fa8a78b
ju310 <ju31094@gmail.com>
2014-12-17 22:12:09 +0100

    Änderung 1_op
    Vielleicht funktioniert es?

--- src/prefix2postfix/prefix2postfix.c
+++ src/prefix2postfix/prefix2postfix.c
@@ -25,16 +25,44 @@ int ist_1_op(char * arr){
     //liest die ersten zusammenhängenden Zeichen ein
     for (i=0; ((*(arr+i)!=' ')&&(i<MAX_OPERATOR)); i++){
         op[i]=*(arr+i);
-        oplength=i;
+        oplength=i+1;
     }
 
     //Alles mit 3 oder mehr Zeichen ist ein Operator, sonst Variable
-    if (oplength>=2){
-        return 1;
+    if (oplength>=3){
+        return oplength;
     }
     else{
         return 0;
     }
+}
+
+char* prefix2postfix (char* string){
+
+    //bestimmt die Länge, -1, da bei fgets der zeilenumbruch mitgezählt wird
+    int strlength= strlen(string)-1;
+    int oplength;
+    int i;
+
+    //holt Speicher für das Postfixarray in arr
+    char* arr= (char *) malloc(strlength*sizeof(char));
+
+    if (ist_2_op(string)){
+        prefix2postfix(arr);
+        prefix2postfix(arr);
+    }
+    if (ist_1_op(string)){
+        oplength=ist_1_op(arr);
+        //schreibt Operator ans Ende
+        *(arr+strlength-oplength-1)=' ';
+        for (i=0; i<oplength; i++){
+            *(arr+strlength-oplength+i)=*(string+i);
+        }
+        //Rest wird als 1 Operand angesehen und rekursiv verarbeitet
+        prefix2postfix(string + oplength+1);
+    }
+
+    return arr;
 }
 
 void write(char postfix[], int length){
@@ -42,26 +70,6 @@ void write(char postfix[], int length){
     for (i=0;i<length;i++){
         printf("%c", *(postfix+i));
     }
-}
-
-char* prefix2postfix (char string[MAX_LENGTH]){
-
-    //bestimmt die Länge, -1, da bei fgets der zeilenumbruch mitgezählt wird
-    int strlength= strlen(string)-1;
-
-    //holt Speicher für das Postfixarray in arr
-    char* arr= (char *) malloc(strlength*sizeof(char));
-    //arr=read(string);
-
-    if (ist_2_op(arr)){
-        prefix2postfix(arr);
-        prefix2postfix(arr);
-    }
-    if (ist_1_op(arr)){
-        prefix2postfix(arr);
-    }
-
-    return arr;
 }
 
 int main(){
@@ -72,8 +80,9 @@ int main(){
 
     //liest die Präfixnotation als String ein
     printf("Bitte gib einen Term in Prefixnotation ein:\n");
-    //bestimmt die Länge, -1, da bei fgets der zeilenumbruch mitgezählt wird
     fgets(string,MAX_LENGTH, stdin);
+
+    //bestimmt die Länge, -1, da bei fgets der zeilenumbruch mitgezählt wird
     length= strlen(string)-1;
 
     //postfix=prefix2postfix(string);
