5f6297f9f1d3f244deefefa7ee16c24c2a3a108a
cathycc <Katharina.klioba@gmx.de>
2015-01-11 13:19:32 +0100

    Codesingle and Decodesingle
    both work perfectly and are commented. Chars that are not letters won't
    be changed when coding/decoding because (de)coding isn't distinct else
    due to backshift.

--- cesartest.c
+++ cesartest.c
@@ -43,8 +43,10 @@ int IsLowerCase(char letter){
 char codesingle(char clear, char key){
 /* function codesingle *****************************************************************************
 **
-** Codesingle codes a clear letter by UpperCase(key)-'A' and returns the coded char.
-** Any char will be accepted for clear, only letters for key, else clear will be returned
+** Codesingle shifts a clear letter by UpperCase(key)-'A' and returns the coded char.
+** If the end of the alphabet is reached, clear will be shifted backwards.
+** Any char will be accepted for clear and key, but if clear is not a letter, it will be returned as it is,
+** if key is not a letter, any clear letter will be returned as it is
 **
 ** input:
 **      char clear: letter that will be coded
@@ -59,7 +61,7 @@ char codesingle(char clear, char key){
     int shift, letternumber;
 
     // initialization of shift depending on whether key is upper or lower case
-    // returns the clear char if key is not a letter*/
+    // returns the clear char if key is not a letter
     if ((IsLowerCase(key) == 0) && (IsUpperCase(key) == 0)){
         printf("Bitte geben Sie einen Buchstaben als Schluessel ein!");
         return clear;
@@ -79,7 +81,7 @@ char codesingle(char clear, char key){
         letternumber = clear-'a';
     }
     if ((IsUpperCase(clear) == 0)&&(IsLowerCase(clear) == 0)){
-        letternumber = 0;
+        return clear;
     }
 
     //returns shifted clear
@@ -92,30 +94,88 @@ char codesingle(char clear, char key){
     }
 } /*codesingle*/
 
+char decodesingle(char code, char key){
+/* function decodesingle *****************************************************************************
+**
+** Decodesingle shifts a code letter by UpperCase(key)-'A' backwards and returns the decoded char.
+** If the end of the alphabet is reached, code will be shifted forwards.
+** Any char will be accepted for code and key, but if code is not a letter, it will be returned as it is,
+** if key is not a letter, any code will be returned as it is
+**
+** input:
+**      char code: letter that will be decoded
+**      char key: Uppercase(key) will be shifted to A
+** output:
+**      char that contains the decoded char
+**
+****************************************************************************************************/
+
+    //shift is the difference between key and a/A
+    //letternumber is the difference between code and a/A, used to check if shifting forwards is neccessary
+    int shift, letternumber;
+
+    // initialization of shift depending on whether key is upper or lower case
+    // returns the code char if key is not a letter*/
+    if ((IsLowerCase(key) == 0) && (IsUpperCase(key) == 0)){
+        printf("Bitte geben Sie einen Buchstaben als Schluessel ein!");
+        return code;
+    }
+    if (IsUpperCase(key)){
+        shift = key - 'A';
+    }
+    if (IsLowerCase(key)){
+        shift = key - 'a';
+    }
+    // initialization of letternumber depending on whether code is uppercase, lowercase or none of both
+    // if code is not a letter, code will be returned
+    if (IsUpperCase(code)){
+        letternumber = code-'A';
+    }
+    if (IsLowerCase(code)){
+        letternumber = code-'a';
+    }
+    if ((IsUpperCase(code) == 0)&&(IsLowerCase(code) == 0)){
+        return code;
+    }
+
+    //returns code shifted backwards
+    if (letternumber>=shift){
+        return code-shift;
+    }
+    //returns code shifted forwards
+    else{
+        return code-shift+26;
+    }
+} /*decodesingle*/
+
 int main(){
 
     int i,j;
-    char clear='A', key='a', code;
+    char clear='A', key='a', code, decode;
 
-    //Tests, if Singlecode works for lower case key letters
-    for (j=0;key<='z';j++){
+    //Tests, if Codesingle and  decodesingle work for lower case key letters
+    for (j=0;key<='e';j++){
         printf("\nSchluesselbuchstabe: %c\n", key);
         for(i=0;clear<='z';i++){
             code=codesingle(clear, key);
-            printf("%c -> %c\t", clear, code);
+            printf("%c -> %c", clear, code);
+            decode=decodesingle(code, key);
+            printf(" -> %c\t", decode);
             clear++;
         }
         key++;
         clear='A';
     }
 
-    //Tests, if Singlecode works for upper case key letters
+    //Tests, if codesingle and decodesingle work for upper case key letters
     key='A';
-    for (j=0;key<='Z';j++){
+    for (j=0;key<='D';j++){
         printf("\nSchluesselbuchstabe: %c\n", key);
         for(i=0;clear<='z';i++){
             code=codesingle(clear, key);
-            printf("%c -> %c\t", clear, code);
+            printf("%c -> %c", clear, code);
+            decode=decodesingle(code, key);
+            printf(" -> %c\t", decode);
             clear++;
         }
         key++;
